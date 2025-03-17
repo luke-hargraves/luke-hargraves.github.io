bbfc54e52b6efdbe01e9ae5115be3f4a626bf0ff
Naveen Gupta <110189741+starryknight620@users.noreply.github.com>
2025-03-17 14:47:21 -0400

    Update scripts.js

--- test/scripts.js
+++ test/scripts.js
@@ -1,10 +1,2 @@
 // the hello world program
 document.write('Hello, World!');
-
-try {
-  let message = "message";
-  message.j = "Some value";  // This will throw a TypeError because 'message' is a string (primitive)
-} catch (error) {
-  // Capture the error with Datadog RUM
-  DD_RUM.addError(error);
-}
